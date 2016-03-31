743ea6d70fed0fe241804cffd845c863857842e6
Nicolas Mayoraz <nic@zapptitude.com>
2016-03-30 22:59:20 -0700

    Adding LoggerSDK.framework

---
+++ LoggerSDK.framework/Headers/LoggerSDK.h
@@ -0,0 +1,19 @@
+//
+//  LoggerSDK.h
+//  LoggerSDK
+//
+//  Created by Vitalii Skorobreshchuk on 3/18/15.
+//  Copyright (c) 2015 MEV. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for LoggerSDK.
+FOUNDATION_EXPORT double LoggerSDKVersionNumber;
+
+//! Project version string for LoggerSDK.
+FOUNDATION_EXPORT const unsigned char LoggerSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LoggerSDK/PublicHeader.h>
+
+#import <LoggerSDK/Logger.h>
